34eecfcd079c31a80959ad319d14ada7b40921e4
Nuo Chen <chenn16@cardiff.ac.uk>
2024-11-17 00:48:42 +0800

    [+] 原生写法, 使用Class封装组件JavaScript行为逻辑

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // JS File
+class Slider {
+  constructor(id) {
+    this.container = document.getElementById(id);
+    this.items = this.container.querySelectorAll(
+      ".slider-list__item, .slider-list__item--selected"
+    );
+  }
+  getSelectedItem() {
+    const selected = this.container.querySelector(
+      ".slider-list__item--selected"
+    );
+    return selected;
+  }
+  getSelectedItemIndex() {
+    return Array.from(this.items).indexOf(this.getSelectedItem());
+  }
+  slideTo(idx) {
+    const selected = this.getSelectedItem();
+    if (selected) {
+      selected.className = "slider-list__item";
+    }
+    const item = this.items[idx];
+    if (item) {
+      item.className = "slider-list__item--selected";
+    }
+  }
+  slideNext() {
+    const currentIdx = this.getSelectedItemIndex();
+    const nextIdx = (currentIdx + 1) % this.items.length;
+    this.slideTo(nextIdx);
+  }
+  slidePrevious() {
+    const currentIdx = this.getSelectedItemIndex();
+    const previousIdx =
+      (this.items.length + currentIdx - 1) % this.items.length;
+    this.slideTo(previousIdx);
+  }
+}
+
+const slider = new Slider("my-slider");
+slider.slideTo(3);
